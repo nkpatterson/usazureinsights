baea7472537977b342ebbf7f8f2ccfebc13c9295
Nick Patterson <nkpatterson@Nicks-iMac.local>
2017-12-01 07:45:01 -0700

    Cleanup

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -82,8 +82,6 @@ export class ReportComponent implements OnInit {
       }
     };
 
-    
-
     this.container = jQuery("#report-container")[0];
     this.report = window.powerbi.embed(this.container, <any>config) as pbi.Report;
 
