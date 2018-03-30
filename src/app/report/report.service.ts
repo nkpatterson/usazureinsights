5199ef4c8c9396112ceefde2fd085262b6a8da70
Nick Patterson <nkpatterson@Nicks-iMac.local>
2018-03-30 11:45:55 -0700

    Fixed ISV Co-Sell report

--- src/app/report/report.service.ts
+++ src/app/report/report.service.ts
@@ -4,10 +4,18 @@ import * as pbi from 'powerbi-client';
 import * as models from 'powerbi-models';
 
 const reportsList: Report[] = [
-    new Report("U.S. Azure Insights Dashboard", "934c259c-235f-48b9-abd2-be79ea1ba5b3", "93dacd31-caed-458a-8c63-6fe12375911f", null, "mailto:nipatter@microsoft.com?subject=U.S. Azure Insights Dashboard Access&body=Please add me :)"),
-    new Report("Azure Monthly MSDN Benefits Usage", "b5d62a77-965f-4839-996d-95f9e0379b91", "93dacd31-caed-458a-8c63-6fe12375911f", new AccountFilterConfig("DimAccounts", "AccountName", "DimAccounts", "TPID")),
-    new Report("ISV Co-Sell Dashboard", "1d9ee913-1bc1-4936-83b2-ccfb81e169eb", "be4af5f0-770e-4e90-b7f0-9e04c864d7a8", new AccountFilterConfig("Account", "TP Account Name", "Partner Sales Connect Deals", "TPID", true), 'http://aka.ms/ocpinsights')
-  ];
+  new Report("U.S. Azure Insights Dashboard", "934c259c-235f-48b9-abd2-be79ea1ba5b3", "93dacd31-caed-458a-8c63-6fe12375911f", null, "mailto:nipatter@microsoft.com?subject=U.S. Azure Insights Dashboard Access&body=Please add me :)"),
+  new Report("Azure Monthly MSDN Benefits Usage", "b5d62a77-965f-4839-996d-95f9e0379b91", "93dacd31-caed-458a-8c63-6fe12375911f", new AccountFilterConfig("DimAccounts", "AccountName", "DimAccounts", "TPID")),
+  new Report("ISV Co-Sell Dashboard", "c7d1b088-c2e2-4b2b-b163-cfe0a682ec21", "be4af5f0-770e-4e90-b7f0-9e04c864d7a8", new AccountFilterConfig("Opportunity", "TP Account Name", "Opportunity", "TPID", true), 'http://aka.ms/ocpinsights', rpt => {
+    rpt.getPages().then(pages => {
+      pages.forEach(page => {
+        if (page.displayName == "Partner IP Co-Sell Summary") {
+          rpt.setPage(page.name);
+        }
+      });
+    });
+  })
+];
 
 @Injectable()
 export class ReportService {
