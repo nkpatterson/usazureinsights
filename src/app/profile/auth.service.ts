f2d6e8e8d4102a417c634804169a9df59a2c59d9
Nick Patterson <nkpatterson@Nicks-iMac.lan>
2019-02-27 15:22:19 -0700

    Updated to latest version of packages including MSAL which no longer prompts for account selection.

--- src/app/profile/auth.service.ts
+++ src/app/profile/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import * as Msal from 'msal';
+import * as Msal from '@azure/msal';
 import { environment } from '../../environments/environment';
 
 declare var appInsights: any;
